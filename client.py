1cedc89d2f341aaf5f324ee380754a4ea4e137f5
jbenaventeheras <you@example.com"n>
2018-10-31 00:12:14 +0100

    añado expires lo paso a server, falta delete y jsonç

--- client.py
+++ client.py
@@ -7,7 +7,7 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
-if len(sys.argv) != 5:
+if len(sys.argv) != 6:
         sys.exit('Usage: client.py ip puerto register' +
                  'sip_address')
 
@@ -15,11 +15,13 @@ SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 Client_user = sys.argv[4]
 Mess_type = sys.argv[3]
+expires_value = sys.argv[5]
 Line_To_Send = ""
 
 
 if Mess_type == 'register':
-    Line_To_Send = ('REGISTER sip:' + Client_user + ' SIP/2.0\r\n'  )
+    Line_To_Send = ('REGISTER sip:' + Client_user + ' SIP/2.0\r\n' +
+'Expires: ' + expires_value + '\r\n\r\n')
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
             my_socket.connect((SERVER, PORT))
             my_socket.send(bytes(Line_To_Send, 'utf-8') + b'\r\n')
--- server.py
+++ server.py
@@ -9,34 +9,49 @@ import json
 import socketserver
 from datetime import datetime, date, time, timedelta
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+if len(sys.argv) != 2:
+    sys.exit('usage error: python3 server.py <port> ')
+else:
+    try:
+        PUERTO = int(sys.argv[1])
+        FORMAT = '%H:%M:%S %d-%m-%Y'
+    except:
+        sys.exit('usage error: python3 server.py <port>')
+
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
 
     def handle(self):
 
-        dicc = {'address': ''}
+        dicc = {'address': '', 'expires': ''}
         list = []
         IP = self.client_address[0]
         PUERTO = self.client_address[1]
 
         for line in self.rfile:
             list.append(line.decode('utf-8'))
-            message = list[0].split(' ')
-        print(message)
+        message = list[0].split(' ')
+        expires = list[1].split(' ')
+        expires_user = expires[1].split('\r\n')[0]
+
 
         if message[0] == 'REGISTER':
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
             user = message[1].split(':')[1]
             dicc['address'] = IP + ' ' + user
-            print(dicc['address'])
+            expires_date = datetime.now() + timedelta(seconds=int(expires_user))
+            dicc['expires'] = expires_date.strftime(FORMAT)
+            print(dicc)
+
+
 
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer(('', PUERTO), SIPRegisterHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
