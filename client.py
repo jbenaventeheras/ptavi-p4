bb71a3f59951ca427f77fbb3d92dbaa40b7e2c73
Javier Benavente Heras <javierbh@alpha29.aulas.gsyc.urjc.es>
2018-11-02 17:33:41 +0100

    pep257 y pycodestyle OK

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente UDP que abre un socket a un servidor
-"""
+"""Programa cliente UDP que abre un socket a un servidor."""
 
 import socket
 import sys
@@ -21,7 +19,7 @@ Line_To_Send = ""
 
 if Mess_type == 'register':
     Line_To_Send = ('REGISTER sip:' + Client_user + ' SIP/2.0\r\n' +
-'Expires: ' + expires_value + '\r\n\r\n')
+                    'Expires: ' + expires_value + '\r\n\r\n')
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
             my_socket.connect((SERVER, PORT))
             my_socket.send(bytes(Line_To_Send, 'utf-8') + b'\r\n')
--- server.py
+++ server.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Clase (y programa principal) para un servidor SIPRegisterHandler
-"""
+"""Clase (y programa principal) para un servidor SIPRegisterHandler."""
 
 import sys
 import json
@@ -12,34 +10,29 @@ from datetime import datetime, date, time, timedelta
 if len(sys.argv) != 2:
     sys.exit('usage error: python3 server.py <port> ')
 else:
-    try:
         PUERTO = int(sys.argv[1])
         FORMAT = '%H:%M:%S %d-%m-%Y'
-    except:
-        sys.exit('usage error: python3 server.py <port>')
+
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+    """Echo server class."""
+
     final_dicc = {}
 
     def register2json(self):
-
+        """JSON."""
         json.dump(self.final_dicc, open('registered.json', 'w'), indent=3)
 
     def json2register(self):
-        """
-        Gets a json file and turns it into a dictionary file
-        """
+        """Get a json file and turns it into a dictionary file."""
         try:
             with open('registered.json', 'r') as file:
-                self.final_dicc= json.load(file)
+                self.final_dicc = json.load(file)
         except (FileNotFoundError, ValueError, json.decoder.JSONDecodeError):
             pass
 
     def handle(self):
-
+        """handle."""
         self.json2register()
         dicc = {'address': '', 'expires': ''}
         list = []
@@ -52,12 +45,11 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         expires = list[1].split(' ')
         expires_user = expires[1].split('\r\n')[0]
 
-
         if message[0] == 'REGISTER':
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
             user = message[1].split(':')[1]
             dicc['address'] = IP + ' ' + user
-            expires_date = datetime.now() + timedelta(seconds=int(expires_user))
+            expires_date = datetime.now()+timedelta(seconds=int(expires_user))
             dicc['expires'] = expires_date.strftime(FORMAT)
 
         if int(expires_user) == 0:
@@ -82,6 +74,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             for user in user_del:
                 del self.final_dicc[user]
         self.register2json()
+
 
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001
